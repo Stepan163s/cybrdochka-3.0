e7e6a6be704c79f4a90552304ca72614002140f5
Stepan163s <109583349+Stepan163s@users.noreply.github.com>
2023-09-26 23:25:36 +0400

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -1,1 +1,18 @@
+from aiogram.types import (
+    KeyboardButton,
+    Message,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
 
+
+
+admin_panel = ReplyKeyboardMarkup(
+    adm = [
+    [
+                    KeyboardButton(text="status"),
+                    KeyboardButton(text="ban_user"),
+                    ]
+                ],
+                resize_keyboard=True,
+            )
